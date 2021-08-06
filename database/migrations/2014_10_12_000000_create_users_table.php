9232879bf3cc9387a8c228a9eb5804347287d862
amir <baghshahyamyr@gmail.com>
2021-08-06 19:04:15 +0430

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->integer("role")->default(2);
             $table->string("status")->default('incomplete-information');
             $table->string("status_cause")->default('احراز هویت انجام نشده');
-            $table->tinyInteger("archive")->default(0);
+            $table->boolean("archive")->default(false);
             $table->string('password');
             $table->timestamps();
         });
