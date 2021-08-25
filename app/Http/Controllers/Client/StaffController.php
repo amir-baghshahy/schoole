f9f360baff2e0690c98d4202b41433d703487781
amir <baghshahyamyr@gmail.com>
2021-08-25 20:17:59 +0430

    fix

--- app/Http/Controllers/Client/StaffController.php
+++ app/Http/Controllers/Client/StaffController.php
@@ -33,50 +33,20 @@ class StaffController extends Controller
         }
 
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'family' => 'required|string|max:255',
-            'degree' => 'required',
-            'teaching_experience' => 'nullable',
-            'major' => 'required',
-            'image' => 'string',
-            'shabanumber' => 'nullable|digits:24',
-            'birthday' => 'required'
+            'id' => 'required',
+            'phone' => 'required|max:11|min:11|unique:users,phone,' . $user->id,
+            'password' => 'required|string|min:8|confirmed',
         ], [
             'phone.unique' => 'شماره قبلا ثبت شده است ',
         ]);
 
 
-        if ($validator->fails()) {
-            return response(['message' => $validator->errors()->first(), 'status' => false], 422);
-        }
-
-        if ($request->file('image')) {
-            $request_data = $this->upload_image($request->only(['name', 'family', 'degree', 'teaching_experience', 'major', 'image', 'shabanumber', 'birthday']));
-        } else {
-            $request_data = $request->only(['name', 'family', 'degree', 'teaching_experience', 'major', 'image', 'shabanumber', 'birthday']);
-        }
-
-        $request_data['id'] = auth()->user()->id;
-        $result = $this->repository->update($request_data);
+        $result = $this->repository->update($request->id,  $request->except(['id']));
 
         if ($result) {
             return response(['status' => true], 200);
         } else {
             return response(['status' => false], 422);
         }
-    }
-
-
-    public function upload_image($request)
-    {
-        $file = $request['image'];
-        $filename = "images/staff/" . time() . '_' . $file->getClientOriginalName();
-        $location = public_path('images/staff');
-        $file->move($location, $filename);
-
-        $request_data = $request;
-        $request_data['image'] = $filename;
-
-        return $request_data;
     }
 }
