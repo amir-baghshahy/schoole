319cb7e0c5c85249754eeab0321451241841901b
amir <baghshahyamyr@gmail.com>
2021-08-25 19:42:21 +0430

    add albume

--- app/Http/Controllers/Admin/AdminAlbumController.php
+++ app/Http/Controllers/Admin/AdminAlbumController.php
@@ -3,9 +3,63 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AlbumResource;
+use App\Models\Album;
+use App\Repositories\AlbumRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AdminAlbumController extends Controller
 {
-    //
+
+    protected $repository;
+
+    public function __construct(AlbumRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function  index()
+    {
+        return new AlbumResource($this->repository->index());
+    }
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'titile' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response(['message' => $validator->errors()->first(), 'status' => false], 422);
+        }
+
+        if ($this->repository->create($request)) {
+            return response(['status' => true]);
+        } else {
+            return response(['status' => false]);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'titile' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response(['message' => $validator->errors()->first(), 'status' => false], 422);
+        }
+
+        if ($this->repository->update($request)) {
+            return response(['status' => true]);
+        } else {
+            return response(['status' => false]);
+        }
+    }
+
+    public function delete($id)
+    {
+    }
 }
--- app/Repositories/AlbumRepository.php
+++ app/Repositories/AlbumRepository.php
@@ -3,7 +3,22 @@
 namespace App\Repositories;
 
 use App\Models\About;
+use App\Models\Album;
 
 class AlbumRepository
 {
+    public function index()
+    {
+        return Album::all();
+    }
+
+    public function create($request)
+    {
+        return Album::create($request);
+    }
+
+    public function update($request)
+    {
+        return Album::find($request->id)->update($request);
+    }
 }
