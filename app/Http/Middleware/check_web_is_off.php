6b6fa917218ccf613d3c17431800f2e8c892ad80
amir <baghshahyamyr@gmail.com>
2021-08-29 13:00:33 +0430

    Update check_web_is_off.php

--- app/Http/Middleware/check_web_is_off.php
+++ app/Http/Middleware/check_web_is_off.php
@@ -23,7 +23,8 @@ class check_web_is_off
         if ($setting->web_mode == 0) {
             return $next($request);
         }elseif ($setting->web_mode == 1) {
-            if(isset(session('user_role')) && session('user_role') == 0){
+            $user_role = session('user_role');
+            if(isset($user_role) && $user_role == 0){
                  return $next($request);
          }else{
                 return response(['message' => 'در حال حاضر وبسایت در دسترس نمی باشد', 'code' => '503'], 503);
