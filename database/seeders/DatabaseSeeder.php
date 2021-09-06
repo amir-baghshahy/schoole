bdfb5ff8f58c1a7e13f6c784f03b59c9e166f643
amir <baghshahyamyr@gmail.com>
2021-09-06 23:31:11 +0430

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,10 +24,12 @@ class DatabaseSeeder extends Seeder
         $user = User::create([
             'phone' => '09140912196',
             'role' => '0',
+            'status' => '',
+            'status_cause' => '',
             'super_user' => true,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
         ]);
 
-        Staff::create(['user_id' => $user->id, 'name' => 'امیر', 'family' => 'باغشاهی', 'rolname' => 'مدیر', 'teaching_experience' => 0, 'major' => 'کامپیوتر', 'image' => '', "birthday" => "1381-12-25", "status" => 0]);
+        Staff::create(['user_id' => $user->id, 'name' => 'امیر', 'family' => 'باغشاهی', 'rolename' => 'مدیر', 'teaching_experience' => 0, 'major' => 'کامپیوتر', "birthday" => "1381-12-25", 'status' => 0]);
     }
 }
