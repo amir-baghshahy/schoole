a1baba5ba2f1163ed3f21f52f9a76f17ec4e1baa
amir <baghshahyamyr@gmail.com>
2021-09-06 23:27:21 +0430

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use App\Models\Setting;
+use App\Models\Staff;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Setting::factory(1)->create();
+        Setting::create([
+            'web_mode' => 0,
+            'register_mode' => 0
+        ]);
+
+        $user = User::create([
+            'phone' => '09140912196',
+            'role' => '0',
+            'super_user' => true,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        ]);
+
+        Staff::create(['user_id' => $user->id, 'name' => 'امیر', 'family' => 'باغشاهی', 'rolname' => 'مدیر', 'teaching_experience' => 0, 'major' => 'کامپیوتر', 'image' => '', "birthday" => "1381-12-25", "status" => 0]);
     }
 }
